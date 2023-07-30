cdcaa656001e49beb7f2dbb0636bcf474a08ba73
Ronald-Joaquim <ronaldjoaquimcardoso11@gmail.com>
2023-07-29 22:13:33 -0300

    Criação dos primeiros itens

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import logo from "./img/Logo.svg";
+
+import styles from "./styles/home.module.css";
+
+import { IoMdAddCircleOutline } from "react-icons/io";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className={styles.logo}>
+        <img src={logo} alt="logo" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className={styles.task}>
+        <input
+          className={styles.textAdd}
+          type="text"
+          placeholder="Adicione uma nova tarefa"
+        />
+        <button className={styles.btnAdd} type="submit">
+          Criar <IoMdAddCircleOutline className={styles.icon} />
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className={styles.container}>
+        <div className={styles.sub}>
+          <div className={styles.texto}>
+            <h5 className={styles.nome1}>Tarefas criadas</h5>
+            <h5 className={styles.num}>0</h5>
+          </div>
+          <div className={styles.texto}>
+            <h5 className={styles.nome2}>Concluídas</h5>
+            <h5 className={styles.num}>0</h5>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
